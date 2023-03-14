dbc6231cb6eee27db1eb063f03ac80306636c2b5
KNobuki <nobuki@hslab.ees.saitama-u.ac.jp>
2023-03-14 10:04:48 +0900

    fix: modify table name

--- initdb.d/smart-mat.sql
+++ initdb.d/smart-mat.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS `smart_mat`;
-
-CREATE TABLE `smart_mat` (
+CREATE TABLE IF NOT EXISTS `smart_mats` (
     `id` INT(10) unsigned NOT NULL AUTO_INCREMENT,
     `current_weight` DOUBLE NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
